f53eba10a5b41116804a89254331ac14402c6788
SherisseTJW <user@example.com>
2025-05-18 14:04:01 +0800

    feat: ggez hello world tutorial

---
+++ hello_ggez/src/main.rs
@@ -0,0 +1,31 @@
+use ggez::*;
+
+struct State {
+    dt: std::time::Duration,
+}
+
+impl ggez::event::EventHandler<GameError> for State {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        self.dt = ctx.time.delta();
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        println!("Hello ggez! dt = {}ns", self.dt.as_nanos());
+        Ok(())
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let state = State {};
+    let c = conf::Conf::new();
+
+    let (ctx, event_loop) = ContextBuilder::new("hello_ggez", "awesome_person")
+        .default_conf(c)
+        .build()
+        .unwrap();
+
+    event::run(ctx, event_loop, state);
+}
